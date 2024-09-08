d637ef64293b50a2e1b5abe241caf1c0273482e9
kickJS22 <kickjs@JBustillos>
2024-09-08 01:25:52 -0300

    Changing var name

--- WorkingWText/text.asm
+++ WorkingWText/text.asm
@@ -1,14 +1,14 @@
 global _start
 
 section .data ; Section to write memory
-	msg db "Hello I'm Juani, wtf is this", 0x0a ; 0x0a is the hex for 410, newline character
-	len equ $ - msg ; We substract the location after msg from inital location
+	whatiwant db "Hello I'm Juani, wtf is this", 0x0a ; 0x0a is the hex for 410, newline character. name db(define byte) text,0x0a
+	len equ $ - whatiwant ; We substract the location after msg from inital location
 
 section .text ; We return to our main section where code will run
 _start:
 	mov eax, 4 ; System call to write, system_write
 	mov ebx, 1 ; We use the file decriptor stdout (1) used to write information in screen, to give an output
-	mov ecx, msg ; Indicates wich bytes we want to write
+	mov ecx, whatiwant ; Indicates wich bytes we want to write
 	mov edx, len ; Indicates the lenght, number of bytes we want to write
 	int 0x80 ; We call the interruptor to make the system call once
 	mov eax, 1 ; System call to exit the program
